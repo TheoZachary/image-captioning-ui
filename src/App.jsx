fcbd9eee4c9b2d71f7a63c31ae2a77b391007ec8
TheoZachary <theozachary2902@gmail.com>
2026-01-10 03:26:53 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     
     setLoading(true);
     try {
-      const response = await fetch('http://10.108.155.52:7860/predict', {
+      const response = await fetch('https://theo1206-image-captioning.hf.space/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
